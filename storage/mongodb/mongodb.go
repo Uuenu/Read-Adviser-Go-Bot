89fc79f21d1c1eabd2d02a72b44b0edab177bd87
Uuenu <nelsonff918@gmail.com>
2022-10-19 22:20:51 +0300

    add mongodb storage

--- storage/mongodb/mongodb.go
+++ storage/mongodb/mongodb.go
@@ -3,8 +3,10 @@ package mongodb
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	lib "telegram-bot-go/lib/e"
 	"telegram-bot-go/storage"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -45,63 +47,63 @@ func (s Storage) Save(page *storage.Page) (err error) {
 	defer func() { err = lib.WrapIfErr("can't save page", err) }()
 
 	userCollection := s.DB.Collection(page.UserName)
-	fmt.Println(userCollection)
+	fmt.Println("user collection", userCollection)
 	if userCollection == nil {
 		s.DB.CreateCollection(nil, page.UserName, nil)
 	}
 	doc := bson.D{{"page_url", page.URL}}
 	result, err := userCollection.InsertOne(context.TODO(), doc)
-	fmt.Println(result)
+	fmt.Println("result save: ", result)
 	return err
 
 }
 
 func (s Storage) IsExist(p *storage.Page) (bool, error) {
-	userUrl, err := s.DB.Collection(p.UserName).Find(s.Ctx, bson.M{"page_url": p.URL})
-	fmt.Println(userUrl, " ", err)
-	// if err != nil {
-	// 	return false, lib.Wrap("can't check data in db", err)
-	// }
-	// if userUrl.Current.String() != "" {
-	// 	return false, nil
-	// }
-	// return true, nil
+	var result bson.M
+	err := s.DB.Collection(p.UserName).FindOne(context.TODO(), bson.M{"page_url": p.URL}).Decode(&result)
+	if err != nil {
+		return false, lib.Wrap("can't get data from db", err)
+	}
+	fmt.Println("result: ", result)
+	link := fmt.Sprintf("%v", result["page_url"])
+	fmt.Println("link and p.URL: ", link, p.URL)
+	if link == p.URL {
+		return true, nil
+	}
 	return false, nil
 }
 
 func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
 
-	// urlDocuments, err := s.DB.Collection(userName).Find(context.TODO(), bson.D{})
-	// if err != nil {
-	// 	return nil, lib.WrapIfErr("can't connect to db", err)
-	// }
+	urlDocuments, err := s.DB.Collection(userName).Find(context.TODO(), bson.D{})
+	if err != nil {
+		return nil, lib.WrapIfErr("can't connect to db", err)
+	}
 
-	// links := make([]string, 0)
+	links := make([]string, 0)
 
-	// for urlDocuments.Next(context.TODO()) {
-	// 	var result bson.M
-	// 	err := urlDocuments.Decode(&result)
-	// 	// If there is a cursor.Decode error
-	// 	if err != nil {
-	// 		return nil, lib.WrapIfErr("cursor.Next() error:", err)
+	for urlDocuments.Next(context.TODO()) {
+		var result bson.M
+		err := urlDocuments.Decode(&result)
+		// If there is a cursor.Decode error
+		if err != nil {
+			return nil, lib.WrapIfErr("cursor.Next() error:", err)
 
-	// 		// If there are no cursor.Decode errors
-	// 	} else {
-	// 		links = append(links, fmt.Sprintf("%v", result["page_url"]))
-	// 	}
-	// }
+			// If there are no cursor.Decode errors
+		} else {
+			links = append(links, fmt.Sprintf("%v", result["page_url"]))
+		}
+	}
 
-	// rand.Seed(time.Now().UnixNano())
-	// n := rand.Intn(len(links))
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(links))
 
-	// link := links[n]
+	link := links[n]
 
-	// var p storage.Page
-	// p.URL = link
+	var p storage.Page
+	p.URL = link
 
-	//return &p, nil
-
-	return nil, nil
+	return &p, nil
 }
 
 func (s Storage) Remove(p *storage.Page) error {
